f91b4a5becf5355891dc4bf061eda8e8d3ffb78a
rowbyourself <fco.cosmellig@gmail.com>
2024-08-29 17:04:42 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'webApp',
+  webDir: 'www'
+};
+
+export default config;
